8652114ec3eb2190f28b16606bab10e1a2035dee
Andy Truong <thehongtt@gmail.com>
2016-06-29 14:57:20 +0700

    Provide a service provider.

---
+++ MiddlewareServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace go1\middleware;
+
+use Pimple\Container;
+use Pimple\ServiceProviderInterface;
+
+class MiddlewareServiceProvider implements ServiceProviderInterface
+{
+    public function register(Container $c)
+    {
+        $c['middleware.portal.name-to-portal'] = function (Container $c) {
+            return new NameToPortalMiddleware($c['cache'], $c['client'], $c['portal_url']);
+        };
+
+        $c['middleware.uuid-to-jwt'] = function (Container $c) {
+            return new UuidToJwtMiddleware($c['cache'], $c['client'], $c['user_url'], $c['private_key']);
+        };
+    }
+}
