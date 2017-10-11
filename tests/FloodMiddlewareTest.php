3bb64d6326ef25fe5030147a2326e1746094de96
Phuong Bui <phuong.bui@go1.com.au>
2017-10-11 09:44:24 +0700

    GO1P-14156: update test case

--- tests/FloodMiddlewareTest.php
+++ tests/FloodMiddlewareTest.php
@@ -2,31 +2,29 @@
 
 namespace go1\report_helpers\tests;
 
-use Aws\S3\S3Client;
 use Doctrine\DBAL\DriverManager;
-use Elasticsearch\Client as ElasticsearchClient;
 use go1\flood\Flood;
 use go1\middleware\FloodMiddleware;
-use go1\report_helpers\Export;
-use go1\report_helpers\ReportHelpersServiceProvider;
 use PHPUnit\Framework\TestCase;
-use Pimple\Container;
 use Symfony\Component\HttpFoundation\Request;
 use Vectorface\Whip\Whip;
 
 class FloodMiddlewareTest extends TestCase
 {
-
     public function testContainerValidation()
     {
         $db = DriverManager::getConnection(['url' => 'sqlite://sqlite::memory:']);
         $flood = new Flood($db, 'flood', new Whip(Whip::PROXY_HEADERS));
         $eventName = 'flood';
         $flood->install();
-        $floodMiddleWare = new FloodMiddleware($flood, 50, 3600, $eventName);
+        $floodMiddleWare = new FloodMiddleware($flood, 3, 3600, $eventName);
         $floodMiddleWare->__invoke(new Request());
         $floodMiddleWare->__invoke(new Request());
-        $floodMiddleWare->__invoke(new Request());
+        $this->assertNull($floodMiddleWare->__invoke(new Request()));
+
+        $response = $floodMiddleWare->__invoke(new Request());
+        $this->assertInstanceOf("Symfony\Component\HttpFoundation\JsonResponse", $response);
+        $this->assertEquals(429, $response->getStatusCode());
         $this->assertEquals(3, $flood->count($eventName));
     }
 }
